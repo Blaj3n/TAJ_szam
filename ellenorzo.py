1a9b2930bf09c4b4551211f4d5bef87c19ac6a67
Blaj3n <benceulrich@gmail.com>
2024-08-18 19:19:51 +0200

    ellenőrzőszám eltárolása változóba

--- ellenorzo.py
+++ ellenorzo.py
@@ -1,1 +1,7 @@
-taj_szam = int(input("Kérem a TAJ-számot: "))
+# CHATGPT: A programod nem fog működni, mivel az int() függvénnyel egész számmá alakítod a bemenetet, és a numerikus
+# típusokon nem lehet indexelni (tehát nem kérheted le a TAJ-szám utolsó számjegyét úgy, ahogy egy string esetén tennéd).
+taj_szam = (input("Kérem a TAJ-számot: "))
+
+utolso_szamjegy = taj_szam[-1]
+#012345678
+print(f"Az ellenőrzőszámjegy: {utolso_szamjegy}  ")
